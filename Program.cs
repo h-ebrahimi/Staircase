ee01080c66f067625738f82555af35ba63af6800
Lenovo <Lenovo@DESKTOP-067GH63>
2021-11-11 20:53:23 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for (var i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"{string.Concat(Enumerable.Repeat(" ", n-i))}{string.Concat(Enumerable.Repeat("#", i))}");
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
